45cd75daf53eac9cc8655229b7aba1f4603903a6
Émile Aublet <emile.aublet@shopify.com>
2021-07-02 15:39:07 -0400

    update

--- src/lib/hook.jsx
+++ src/lib/hook.jsx
@@ -2,7 +2,8 @@ import { useContext } from "react";
 import { FlagsContext } from "./context";
 export const useFlags = (flag) => {
   const context = useContext(FlagsContext);
-  const match = context?.find((f) => f.name === flag);
+  if (!context) return false;
+  const match = context.find((f) => f.name === flag);
   if (!match) return false;
   const { on, ...rest } = match;
   return on ? { on, ...rest } : false;
